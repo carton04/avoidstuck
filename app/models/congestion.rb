26ad859824ba03f511e6cce251e71f53905e5296
carton04 <114985496+carton04@users.noreply.github.com>
2022-11-24 15:24:21 +0900

    congestionsテーブル作成

---
+++ app/models/congestion.rb
@@ -0,0 +1,9 @@
+class Prototype < ApplicationRecord
+  validates :ski_resort_id, presence: true
+  validates :waiting_id, presence: true
+  validates :image, presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
+end
---
+++ db/migrate/20221124061319_create_congestions.rb
@@ -0,0 +1,11 @@
+class CreateCongestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :congestions do |t|
+      t.integer :ski_resort_id, null: false
+      t.text :situation
+      t.integer :waiting_id,    null: false
+      t.references :user,       null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
