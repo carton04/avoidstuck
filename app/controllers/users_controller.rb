fe4cd6ddce82633871565ac26140ac7bc353d4fc
carton04 <114985496+carton04@users.noreply.github.com>
2022-12-04 17:49:09 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @congestions = @user.congestions.order("created_at DESC")
+  end
+
   def create
   end
 end
