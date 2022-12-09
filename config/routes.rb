c87ceadf110c029bcc8287b40046ac7243c33e6d
carton04 <114985496+carton04@users.noreply.github.com>
2022-12-09 20:23:17 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'congestions#index'
   resources :congestions do
-    resources :comments, only: [:create :destroy]
-    resources :solutions, only: [:create :destroy]
+    resources :comments, only: [:create, :destroy]
+    resources :solutions, only: [:create, :destroy]
   end
   resources :users, only: [:show]
 end
