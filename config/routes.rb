64561f231b9d6f0d7af91bbcbc71d20f26c72fc8
carton04 <114985496+carton04@users.noreply.github.com>
2022-12-09 20:21:24 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'congestions#index'
   resources :congestions do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create :destroy]
+    resources :solutions, only: [:create :destroy]
   end
   resources :users, only: [:show]
 end
